4db4ca051ba6c63e1bfaaef9686c28f902beace9
Debby <102255380+Eddy-cs@users.noreply.github.com>
2022-07-18 19:29:19 -0400

    Added styling

--- script.js
+++ script.js
@@ -1,28 +1,28 @@
 // let sprints = Array.from(document.querySelectorAll('.sprint'))
 // addEventListener("onClick" )
 
-let sprints = [...(document.querySelectorAll('.sprint'))];
+let sprints = [...document.querySelectorAll(".sprint")];
 let time = 3000;
 let imageCounter = 0;
 
 sprints[imageCounter].style.opacity = 1;
 
-setInterval(imageSlide, time)
+setInterval(imageSlide, time);
 
-function imageSlide(){
-    sprints[imageCounter].style.animation = "";
-    sprints[imageCounter].style.opacity = 0;
-    imageCounter = (imageCounter + 1) % sprints.length;
-    sprints[imageCounter].style.animation = "300ms pageLoad--left ease-in";
-    sprints[imageCounter].style.opacity = 1;
+function imageSlide() {
+  sprints[imageCounter].style.animation = "";
+  sprints[imageCounter].style.opacity = 0;
+  imageCounter = (imageCounter + 1) % sprints.length;
+  sprints[imageCounter].style.animation = "300ms pageLoad--left ease-in";
+  sprints[imageCounter].style.opacity = 1;
 }
 
-let menu = document.querySelector('.navbar__menu');
-let links = document.querySelector('.navbar__mobile');
+let menu = document.querySelector(".navbar__menu");
+let links = document.querySelector(".navbar__mobile");
 
-menu.addEventListener('click', ()=>{
-    console.log(links.style.display)
-    links.style.display === 'none' ?
-    links.style.display = 'grid' :
-    links.style.display = 'none'
-})
+menu.addEventListener("click", () => {
+  //   console.log(links.style.display);
+  links.style.display === "none"
+    ? (links.style.display = "grid")
+    : (links.style.display = "none");
+});
